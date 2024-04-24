65e570ae42849cae89631fe2779f30ab7091e42e
ylian121 <yongyanliang05@gmail.com>
2024-04-24 08:03:10 -0700

    debug.cpp

--- debug.cpp
+++ debug.cpp
@@ -1,15 +1,15 @@
 #include <iostream> 
-#include <cstring> 
+#include <cstring>
 
-using namespace std; 
+using namespace std;
 
-int main() { 
-    char input[50]; 
+int main() {
+    char* input = NULL; 
     int i = 42; 
-    cin.getline(input, 50); 
-    for (i = strlen(input); i >= 0; i--) {  
-        cout << input[i]; 
-        }  
-    cout << endl; 
-    return 0; 
+    cin.getline(input, 50);
+    for (i = strlen(input); i >= 0; i--) { 
+        cout << input[i];
     }
+    cout << endl; 
+    return 0;
+}
